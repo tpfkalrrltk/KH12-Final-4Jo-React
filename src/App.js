a8f1a52a61e9074fe0397363158a5a3fb86454a6
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-11-08 02:41:34 +0900

    초기 세팅

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Menu from './components/Menu.js';
+
+function App() {
+  return (
+    <div className='container-fluid my-5 py-5'>
+      <Menu />
+    </div>
+  );
+}
+
+export default App;
