98db3089a375b877cc22fb017248d506798ecb26
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-11-09 04:03:26 +0900

    검색 및 li 색상 수정

--- everyfit-react/src/components/Header.js
+++ everyfit-react/src/components/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
 
   return (
     <>
-      <nav class="navbar navbar-expand-lg bg-primary fixed-top p-0" data-bs-theme="light">
+      <nav class="navbar navbar-expand-lg bg-primary fixed-top p-0" data-bs-theme="dark">
         <div class="container-fluid col-9">
           <a class="navbar-brand ms-4" href="/"><img src={logo} width="110px" height="100px" /></a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
@@ -21,7 +21,7 @@ const Header = () => {
 
               <li class="nav-item col-9">
                 <form class="d-flex ">
-                  <input class="form-control me-sm-2 bg-light ms-5 col-8" type="search" placeholder="검색어를 입력해주세요" />
+                  <input class="form-control me-sm-2 bg-light ms-5 col-8 text-primary" type="search" placeholder="검색어를 입력해주세요" />
                   <button class="btn btn-light my-2 my-sm-0 bg-light text-primary" type="submit">Search</button>
                 </form>
               </li>
@@ -47,12 +47,12 @@ const Header = () => {
 
             <li class="nav-item dropdown col-5 ">
               <a class="nav-link dropdown-toggle text-light mt-2 me-5 pe-5" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><h5><FaList /></h5></a>
-              <div class="dropdown-menu">
-                <a class="dropdown-item" href="#">모임 목록</a>
-                <a class="dropdown-item" href="#">리그 목록</a>
-                <a class="dropdown-item" href="#">여성 전용 모임</a>
+              <div class="dropdown-menu bg-primary ">
+                <a class="dropdown-item text-light" href="#">모임 목록</a>
+                <a class="dropdown-item text-light" href="#">리그 목록</a>
+                <a class="dropdown-item text-light" href="#">여성 전용 모임</a>
                 <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">특별 기능</a>
+                <a class="dropdown-item text-light" href="#">특별 기능</a>
               </div>
             </li>
 
