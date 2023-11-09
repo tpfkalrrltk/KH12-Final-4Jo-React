b8ed891f377625d99653c3df1fc79a3c47213d59
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-11-09 16:39:25 +0900

    테스트

--- everyfit-react/src/components/Footer.js
+++ everyfit-react/src/components/Footer.js
@@ -14,15 +14,15 @@ const Footer = () => {
 
   const languageText = isPc ?
 
-    <nav class="fixed-bottom bg-primary p-0">
+    <nav className="fixed-bottom bg-primary p-0">
 
-      <div class="container p-0">
+      <div className="container p-0">
         <div className='row ms-5'>
           <div className=' col-6 '>
-            <p class=" text-light"><small>COMPANY INFO.<br />COMPANY : EVERY FIT<br />ADDRESS : 07212 서울특별시 영등포구 양평동4가 2 길</small></p>
+            <p className=" text-light"><small>COMPANY INFO.<br />COMPANY : EVERY FIT<br />ADDRESS : 07212 서울특별시 영등포구 양평동4가 2 길</small></p>
           </div>
           <div className='col-6 '>
-            <p class=" text-light"><small>CS CENTER.<br />TEL 02-6055-9955<br />OPEN AM 09:00 ~ CLOSE PM 07:00 / SAT.SUN.HOLIDAY CLOSED</small></p>
+            <p className=" text-light"><small>CS CENTER.<br />TEL 02-6055-9955<br />OPEN AM 09:00 ~ CLOSE PM 07:00 / SAT.SUN.HOLIDAY CLOSED</small></p>
           </div>
         </div>
 
